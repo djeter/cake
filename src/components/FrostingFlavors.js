be8bff27ce04270e01a47c89b08b4d32bdcfd090
djeter <djeter@asoundstrategy.com>
2022-07-31 18:34:55 -0400

    1 file modified

--- src/components/FrostingFlavors.js
+++ src/components/FrostingFlavors.js
@@ -9,19 +9,25 @@ export default function FrostingFlavors({
   curFrostingFlavor,
   idx,
 }) {
-  let test = frostings[idx - 1].flavors.map((frosting, index) => (
-    <option key={index} value={frosting}>
-      {frosting}
-    </option>
-  ));
-  return (
-    <>
-      <Field name="Frosting_Flavors" component="select" ref={curFrostingFlavor}>
-        <option value="">Select One</option>
-        {test}
-        <option value="">Skip</option>
-      </Field>
-      <Error name="Frosting_Flavors" />
-    </>
-  );
+  try {
+    let test = frostings[idx - 1].flavors.map((frosting, index) => (
+      <option key={index} value={frosting}>
+        {frosting}
+      </option>
+    ));
+    return (
+      <>
+        <Field
+          name="Frosting_Flavors"
+          component="select"
+          ref={curFrostingFlavor}
+        >
+          <option value="">Select One</option>
+          {test}
+          <option value="">Skip</option>
+        </Field>
+        <Error name="Frosting_Flavors" />
+      </>
+    );
+  } catch (err) {}
 }
